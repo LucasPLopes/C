82ab9822b9d6500e79e1b7eba2ba8ec4096d77a1
Lucas Lopes <l.lopes.fga@gmail.com>
2019-08-23 09:53:14 -0300

    add question in a comment

--- algorithms/insertionsort.c
+++ algorithms/insertionsort.c
@@ -1,30 +1,34 @@
-#include<stdio.h>
+#include <stdio.h>
 
-
-void insertionsort(int *v, int n){
-    int i,j, aux;
-    for( i  = 1; i < n; i++)
+void insertionsort(int *v, int n)
+{
+    int i, j, aux;
+    for (i = 1; i < n; i++)
     {
         j = i;
-        while((j != 0 )&&(v[j] < v[j -1])){
+        while ((j != 0) && (v[j] < v[j - 1]))
+        {
             aux = v[j];
             v[j] = v[j - 1];
             v[j - 1] = aux;
             j--;
-         }
+        }
     }
 }
 
-int main(){
-    int v[10] = {5,2,1,4,3,6,8,9,7,10}, 
-        length = sizeof(v)/sizeof(int);
 
-    insertionsort(v,length);
+// o algoritmo do insertionsort é estável, explique.
+int main()
+{
+    int v[10] = {5, 2, 1, 4, 3, 6, 8, 9, 7, 10},
+        length = sizeof(v) / sizeof(int);
+
+    insertionsort(v, length);
 
     for (size_t i = 0; i < length; i++)
-    {   
+    {
         printf("%i\t", v[i]);
     }
-        printf("\n");
+    printf("\n");
     return 0;
 }
